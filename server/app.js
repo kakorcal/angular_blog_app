6f5256e33ad3e6aaeaed6fba3b3cfa50dc21f14f
Kenneth Korcal <kakorcal@gmail.com>
2016-06-18 18:06:14 -0700

    initial api setup

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const routes = require('./routes/index');
 
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use();
 app.use();
 app.use();
+app.use('/api', routes.blogs);
 
 app.get('*', (req, res)=>{
   res.sendFile('index.html', {root: '../client'});
